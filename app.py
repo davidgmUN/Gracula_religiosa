0ee9ec3f1634c751ae774c5114be3f7b0eb5b9ae
davidgmUN <davidgaravito@uninorte.edu.co>
2021-10-23 01:56:55 -0500

    Sesiones y login_required. Contraseña admin1: Admin1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, jsonify, redirect, request, url_for, flash
+import functools
+from flask import Flask, render_template, jsonify, redirect, request, url_for, flash, session
 from markupsafe import escape
 from werkzeug.security import generate_password_hash, check_password_hash
 from db import consulta, cambios
@@ -13,14 +14,17 @@ app.secret_key=os.urandom(24)
 def login():
     try:
         if request.method=="POST":
+            session.clear()
             usuario=escape(request.form["usuario"])
             contraseña=escape(request.form["contraseña"])
             #Realizar la consulta en la base de datos de si existe o no el usuario registrado
-            consultar=f"Select usuario, contraseña from usuarios where usuario='{usuario}'"
+            consultar=f"Select usuario, contraseña, rol from usuarios where usuario='{usuario}'"
             rta=consulta(consultar)
             if rta!=None:
                 if check_password_hash(rta[1],contraseña):
-                    return redirect(url_for("front_page"),nombre_usuario=usuario)
+                    session["usuario"]=usuario
+                    session["rol"]=rta[2]
+                    return redirect(f"/index_{usuario}")
                 else:
                     flash("Acceso denegado. Contraseña incorrecta.")
                     return redirect('/')
@@ -28,16 +32,30 @@ def login():
                 flash("Usuario no existe, debe registrarse.")
                 return redirect('/')
         else:
-            return render_template('login.html')
+            if session["usuario"]!=None:
+                usuario=session["usuario"]
+                return redirect(f"/index_{usuario}")
+            else:
+                return render_template('login.html')
     except:
         return render_template('login.html')
-    
-@app.route('/index<nombre_usuario>/', methods=("GET","PUT"))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if session["usuario"] is None:
+            return redirect("/login")
+        return view(**kwargs)
+    return wrapped_view
+
+@app.route('/index_<nombre_usuario>/', methods=("GET","PUT"))
+@login_required
 def front_page(nombre_usuario):
     return render_template('frontpage.html')
     
-@app.route('/publicacion/', methods=('GET','POST','PUT','DELETE'))
-def publicacion():
+@app.route('/publicacion_<nombre_usuario>/', methods=('GET','POST','PUT','DELETE'))
+@login_required
+def publicacion(nombre_usuario):
     return render_template('publicacion.html')
     
 @app.route('/registro/', methods=("GET","POST"))
@@ -76,21 +94,28 @@ def registro():
     except:
         return render_template('registro.html')
     
-@app.route('/perfil/', methods=('GET','POST','DELETE','PATCH'))
-def perfil_usuario():
+@app.route('/perfil_<nombre_usuario>/', methods=('GET','POST','DELETE','PATCH'))
+@login_required
+def perfil_usuario(nombre_usuario):
     return render_template('perfil.html')
     
-"""@app.route('/buscar/', methods=("GET"))
+@app.route('/buscar/')
+@login_required
 def buscar_usuario():
     return render_template('busqueda_Usuario.html')
     
-@app.route('/preguntas_frecuentes/', methods=("GET"))
+@app.route('/preguntas_frecuentes/')
 def preguntas_frecuentes():
     return render_template('preguntas_frecuentes.html')
     
 @app.route('/dashboard/', methods=("GET","POST","PUT","DELETE","PATCH","CONNECT"))
+@login_required
 def dashboard():
-    return render_template('dashboard.html')"""
+    if session["rol"]=="Administrador":
+        return render_template('dashboard.html')
+    else:
+        usuario=session["usuario"]
+        return redirect(f"/index_{usuario}")
     
 if __name__ =='__main__':
     app.run(port=80, debug=True)
